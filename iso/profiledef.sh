ce8a6c04b08f8410f2328a112aab5a3c4e1df351
bouddha-fr <bouddha.whitehat@gmail.com>
2025-04-25 16:05:44 +0200

    great push

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archimede"
-iso_label="AOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Archimede by 0xbouddha"
-iso_application="Archimede Linux Live/Rescue DVD"
+iso_name="archlinux"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -19,7 +19,6 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
-  ["/etc/skel/.xinitrc"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
--- iso/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo mkarchiso -v -w ./work -o ./out . 
