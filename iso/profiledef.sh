c977ba77b377dbbfa8039deb9450d32c31cc41f8
bouddha-fr <bouddha.whitehat@gmail.com>
2025-05-06 09:43:28 +0200

    delete custom sh

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -15,7 +15,6 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-airootfs_image_script="customize_airootfs.sh"
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -26,5 +25,4 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/customize_airootfs.sh"]="0:0:755"
 )
